6f463bd4df9fdfe0f4466d9b2d89248b0ad891b0
sunwng <sunwgna@gmail.com>
2024-03-10 15:42:18 +0900

    init

---
+++ src/main/java/org/example/studyspringbatch/StudySpringBatchApplication.java
@@ -0,0 +1,13 @@
+package org.example.studyspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/studyspringbatch/StudySpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.studyspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpringBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
